b40bcf82407c0a50bfb3fc0fee619bf71cf1eb88
tbaveja <tbaveja@gmail.com>
2019-03-25 18:53:08 +0530

    Add files via upload

---
+++ build/precache-manifest.aa2dfc6930b125a0eb6b67c25f1e5bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4edd2674ab1e4686d38f",
+    "url": "/static/js/main.fc8af79e.chunk.js"
+  },
+  {
+    "revision": "4e76f470c8688e0f0761",
+    "url": "/static/js/2.6c563ac0.chunk.js"
+  },
+  {
+    "revision": "4edd2674ab1e4686d38f",
+    "url": "/static/css/main.4b41ca93.chunk.css"
+  },
+  {
+    "revision": "edf1853a630fe455aaba471bb6e623ad",
+    "url": "/index.html"
+  }
+];
